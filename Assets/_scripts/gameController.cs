e9d804e60f7f0b2f3d7dfaa03f17d5f312971c5a
reibeatall <reibeatall@gmail.com>
2014-08-09 18:47:22 -0700

    reset
    reset the game

--- Assets/_scripts/gameController.cs
+++ Assets/_scripts/gameController.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class gameController : MonoBehaviour
 {
 
-		public GameObject blueObj;
-		public GameObject redObj;
-		public GameObject yellowObj;
+	public GameObject blueObj;
+	public GameObject redObj;
+	public GameObject yellowObj;
+	public bool gameOver = false;
 //	 GameObject topPiece;
-		//private midPiece;
-		//private botPiece;
-		//private goalPiece;
+	//private midPiece;
+	//private botPiece;
+	//private goalPiece;
 
 
 //	private void spawnPiece ( GameObject piece, float xPos, float yPos ) 
@@ -25,33 +26,33 @@ public class gameController : MonoBehaviour
 //		spawnPiece (piece, xPos, yPos);
 //		piece.GetComponent<pieceController>().goal = true;
 
-		//piece.renderer.sharedMaterial.color = Color.white;
+	//piece.renderer.sharedMaterial.color = Color.white;
 //	}
 
-		// Use this for initialization
-		void Start ()
-		{
-				GameObject topPiece = Instantiate (blueObj) as GameObject;
-				topPiece.transform.position = new Vector3 (-3, 3, 0);
-				GameObject goal = Instantiate (blueObj) as GameObject;
-				goal.GetComponent<pieceController> ().goal = true;
-				goal.transform.position = new Vector3 (3, 0, 0);
-				//	goal.collider2D.isTrigger = true;
-				GameObject midPiece = Instantiate (redObj) as GameObject;
-				midPiece.transform.position = new Vector3 (-3, 0, 0);
-				GameObject botPiece = Instantiate (yellowObj) as GameObject;
-				botPiece.transform.position = new Vector3 (-3, -3, 0);
+	// Use this for initialization
+	void Start ()
+	{
+		GameObject topPiece = Instantiate (blueObj) as GameObject;
+		topPiece.transform.position = new Vector3 (-3, 3, 0);
+		GameObject goal = Instantiate (blueObj) as GameObject;
+		goal.GetComponent<pieceController> ().goal = true;
+		goal.transform.position = new Vector3 (3, 0, 0);
+		//	goal.collider2D.isTrigger = true;
+		GameObject midPiece = Instantiate (redObj) as GameObject;
+		midPiece.transform.position = new Vector3 (-3, 0, 0);
+		GameObject botPiece = Instantiate (yellowObj) as GameObject;
+		botPiece.transform.position = new Vector3 (-3, -3, 0);
 
-				//			spawnPiece(blueObj, -3, 3);
+		//			spawnPiece(blueObj, -3, 3);
 //			spawnPiece (redObj, -3, 0);
 //		spawnPiece (yellowObj, -3, -3);
 //		spawnGoal (blueObj, 3, 0);
-				//Instantiate (pieceobj, new Vector3 (10, 10, 0), Quaternion.identity);
-		}
+		//Instantiate (pieceobj, new Vector3 (10, 10, 0), Quaternion.identity);
+	}
 	
-		// Update is called once per frame
-		void Update ()
-		{
-	
-		}
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
 }
--- Assets/_scripts/pieceController.cs
+++ Assets/_scripts/pieceController.cs
@@ -4,65 +4,66 @@ using System.Collections;
 public class pieceController : MonoBehaviour
 {
 
-		public bool goal = false;
+	public bool goal = false;
 
-		bool stopDragging;
-		bool drag;
-		bool isMouseUp = false;
-		Vector3 offset;
+	bool stopDragging;
+	bool drag;
+	bool isMouseUp = false;
+	Vector3 offset;
 
-		// Use this for initialization
-		void Start ()
-		{
-				Initalize ();
-		}
+	// Use this for initialization
+	void Start ()
+	{
+		Initalize ();
+	}
 	
-		// Update is called once per frame
-		void Update ()
-		{
+	// Update is called once per frame
+	void Update ()
+	{
 	
+	}
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		Debug.Log ("collision!!");
+		if (drag == true) {
+			Destroy (other.gameObject);
+			Application.LoadLevel (0);
 		}
-		void OnTriggerEnter2D (Collider2D other)
-		{
-				Debug.Log ("collision!!");
-				if (drag == true) {
-						Destroy (other.gameObject);
-				}
+	}
+
+	void Initalize ()
+	{
+		stopDragging = false;
+		drag = false;
+
+	}
+	void OnMouseDown ()
+	{
+		if (goal == false) {
+			isMouseUp = false;
+			drag = true;
+
+			offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0));
 		}
+	}
 
-		void Initalize ()
-		{
-				stopDragging = false;
-				drag = false;
+	void OnMouseDrag ()
+	{
+		if (goal == false) {
+			if (drag && !stopDragging) {
+				Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0);
+				Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint) + offset;
+				transform.position = curPosition;
 
+			}
 		}
-		void OnMouseDown ()
-		{
-				if (goal == false) {
-						isMouseUp = false;
-						drag = true;
+	}
 
-						offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0));
-				}
+	void OnMouseUp ()
+	{
+		if (goal == false) {
+			drag = false;
+			isMouseUp = true;
 		}
-
-		void OnMouseDrag ()
-		{
-				if (goal == false) {
-						if (drag && !stopDragging) {
-								Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0);
-								Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint) + offset;
-								transform.position = curPosition;
-
-						}
-				}
-		}
-
-		void OnMouseUp ()
-		{
-				if (goal == false) {
-						drag = false;
-						isMouseUp = true;
-				}
-		}
+	}
 }
