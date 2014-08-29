64e6b1a1ddec249e59a6542cf6db47f9bc381e2d
reibeatall <reibeatall@gmail.com>
2014-08-28 22:23:18 -0700

    objects loaded via array, goal populated from loaded object. Goal is random. Need to get top/mid/bot spawns also random

--- Assets/_scripts/gameController.cs
+++ Assets/_scripts/gameController.cs
@@ -4,14 +4,17 @@ using System.Collections;
 public class gameController : MonoBehaviour
 {
 
-	public GameObject blueObj;
-	public GameObject redObj;
-	public GameObject yellowObj;
-	public bool gameOver = false;
+		public bool gameOver = false;
+		public Vector3 topSpawn;
+		public Vector3 midSpawn;
+		public Vector3 botSpawn;
+		public Vector3 goalSpawn;
+		public GameObject[] blockPool = new GameObject[3];
+		private GameObject[] goalPool = new GameObject[3];
 //	 GameObject topPiece;
-	//private midPiece;
-	//private botPiece;
-	//private goalPiece;
+		//private midPiece;
+		//private botPiece;
+		//private goalPiece;
 
 
 //	private void spawnPiece ( GameObject piece, float xPos, float yPos ) 
@@ -26,33 +29,36 @@ public class gameController : MonoBehaviour
 //		spawnPiece (piece, xPos, yPos);
 //		piece.GetComponent<pieceController>().goal = true;
 
-	//piece.renderer.sharedMaterial.color = Color.white;
+		//piece.renderer.sharedMaterial.color = Color.white;
 //	}
 
-	// Use this for initialization
-	void Start ()
-	{
-		GameObject topPiece = Instantiate (blueObj) as GameObject;
-		topPiece.transform.position = new Vector3 (-3, 3, 0);
-		GameObject goal = Instantiate (blueObj) as GameObject;
-		goal.GetComponent<pieceController> ().goal = true;
-		goal.transform.position = new Vector3 (3, 0, 0);
-		//	goal.collider2D.isTrigger = true;
-		GameObject midPiece = Instantiate (redObj) as GameObject;
-		midPiece.transform.position = new Vector3 (-3, 0, 0);
-		GameObject botPiece = Instantiate (yellowObj) as GameObject;
-		botPiece.transform.position = new Vector3 (-3, -3, 0);
+		// Use this for initialization
+		void Start ()
+		{
+				GameObject topPiece = Instantiate (blockPool [0]) as GameObject;
+				topPiece.transform.position = topSpawn;
+				GameObject midPiece = Instantiate (blockPool [1]) as GameObject;
+				midPiece.transform.position = midSpawn;
+				GameObject botPiece = Instantiate (blockPool [2]) as GameObject;
+				botPiece.transform.position = botSpawn;
+				goalPool [0] = topPiece;
+				goalPool [1] = midPiece;
+				goalPool [2] = botPiece;
+				Debug.Log (goalPool [0]);
+				GameObject goal = Instantiate (goalPool [Random.Range (0, 3)]) as GameObject;
+				goal.GetComponent<pieceController> ().goal = true;
+				goal.transform.position = goalSpawn;
 
-		//			spawnPiece(blueObj, -3, 3);
+				//			spawnPiece(blueObj, -3, 3);
 //			spawnPiece (redObj, -3, 0);
 //		spawnPiece (yellowObj, -3, -3);
 //		spawnGoal (blueObj, 3, 0);
-		//Instantiate (pieceobj, new Vector3 (10, 10, 0), Quaternion.identity);
-	}
+				//Instantiate (pieceobj, new Vector3 (10, 10, 0), Quaternion.identity);
+		}
 	
-	// Update is called once per frame
-	void Update ()
-	{
+		// Update is called once per frame
+		void Update ()
+		{
 		
-	}
+		}
 }
