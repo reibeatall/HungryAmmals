d8620b3010433d049bec37b3222f7cd126b3b915
reibeatall <reibeatall@gmail.com>
2014-08-31 19:59:43 -0700

    FINALLY
    Got all objects into arrays, being pulled out too

--- Assets/_scripts/gameController.cs
+++ Assets/_scripts/gameController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class gameController : MonoBehaviour
 {
@@ -9,51 +10,30 @@ public class gameController : MonoBehaviour
 		public Vector3 midSpawn;
 		public Vector3 botSpawn;
 		public Vector3 goalSpawn;
-		public GameObject[] blockPool = new GameObject[3];
-		private GameObject[] goalPool = new GameObject[3];
-//	 GameObject topPiece;
-		//private midPiece;
-		//private botPiece;
-		//private goalPiece;
-
-
-//	private void spawnPiece ( GameObject piece, float xPos, float yPos ) 
-//	{
-//		newPiece = Instantiate (piece);
-//		piece.transform.position = new Vector3 (xPos, yPos, 0);
-//		}
-
-//	private void spawnGoal ( GameObject piece, float xPos, float yPos)
-//	{
-
-//		spawnPiece (piece, xPos, yPos);
-//		piece.GetComponent<pieceController>().goal = true;
-
-		//piece.renderer.sharedMaterial.color = Color.white;
-//	}
+		public List<GameObject> blockPool;
+		public List<GameObject> goalPool;
 
 		// Use this for initialization
 		void Start ()
 		{
-				GameObject topPiece = Instantiate (blockPool [0]) as GameObject;
-				topPiece.transform.position = topSpawn;
-				GameObject midPiece = Instantiate (blockPool [1]) as GameObject;
-				midPiece.transform.position = midSpawn;
-				GameObject botPiece = Instantiate (blockPool [2]) as GameObject;
-				botPiece.transform.position = botSpawn;
-				goalPool [0] = topPiece;
-				goalPool [1] = midPiece;
-				goalPool [2] = botPiece;
-				Debug.Log (goalPool [0]);
-				GameObject goal = Instantiate (goalPool [Random.Range (0, 3)]) as GameObject;
-				goal.GetComponent<pieceController> ().goal = true;
-				goal.transform.position = goalSpawn;
-
-				//			spawnPiece(blueObj, -3, 3);
-//			spawnPiece (redObj, -3, 0);
-//		spawnPiece (yellowObj, -3, -3);
-//		spawnGoal (blueObj, 3, 0);
-				//Instantiate (pieceobj, new Vector3 (10, 10, 0), Quaternion.identity);
+				goalPool.Capacity = 3;
+				SpawnRandBlock (topSpawn, false);
+				SpawnRandBlock (midSpawn, false);
+				SpawnRandBlock (botSpawn, false);
+				SpawnRandBlock (goalSpawn, true);
+				//GameObject topPiece = Instantiate (blockPool [0]) as GameObject;
+				//topPiece.transform.position = topSpawn;
+				//GameObject midPiece = Instantiate (blockPool [1]) as GameObject;
+				//midPiece.transform.position = midSpawn;
+				//GameObject botPiece = Instantiate (blockPool [2]) as GameObject;
+				//botPiece.transform.position = botSpawn;
+				//goalPool [0] = topPiece;
+				//goalPool [1] = midPiece;
+				//goalPool [2] = botPiece;
+				//Debug.Log (goalPool [0]);
+				//GameObject goal = Instantiate (goalPool [Random.Range (0, 3)]) as GameObject;
+				//goal.GetComponent<pieceController> ().goal = true;
+				//goal.transform.position = goalSpawn;
 		}
 	
 		// Update is called once per frame
@@ -61,4 +41,23 @@ public class gameController : MonoBehaviour
 		{
 		
 		}
+		
+		void SpawnRandBlock (Vector3 spawn, bool goal)
+		{
+				if (goal == false) {
+						int RandIndex = Random.Range (0, blockPool.Count);
+						GameObject piece = Instantiate (blockPool [RandIndex]) as GameObject;
+						piece.transform.position = spawn;
+						goalPool.Add (blockPool [RandIndex]);
+						blockPool.Remove (blockPool [RandIndex]);
+				} else {
+						int RandIndex = Random.Range (0, goalPool.Count);
+						GameObject piece = Instantiate (goalPool [RandIndex]) as GameObject;
+						piece.transform.position = spawn;
+						piece.GetComponent<pieceController> ().goal = true;
+
+				
+				}
+					
+		} 
 }
