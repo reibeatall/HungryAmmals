8526feafb05f3075181c6f110ae8d3da3b043cf2
reibeatall <reibeatall@gmail.com>
2015-01-11 21:53:04 -0800

    Timer
    Added timer, timer level, restarting level after timer finishes

---
+++ Assets/_scripts/TimerText.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerText : MonoBehaviour
+{
+
+		public Text timerText;
+		public float timeRemaining = 60;
+		
+		// Use this for initialization
+	
+	
+		void Start ()
+		{
+				timerText = GetComponent<Text> ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				timeRemaining -= Time.deltaTime;
+				timerText.text = "Time left: " + (int)timeRemaining;
+				
+				if (timeRemaining <= 0) {
+						Debug.Log ("time's up!");
+
+						Application.LoadLevel (0);
+				}
+		}
+
+
+}
+
+
---
+++ Assets/_scripts/countdownTimer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class countdownTimer : MonoBehaviour
+{
+
+		public float timeRemaining = 60;
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				timeRemaining -= Time.deltaTime;
+		}
+}
